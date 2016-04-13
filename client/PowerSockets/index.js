68a3508123d9fd9c8bd3a3f630d0bb01bcd41ac4
Dan Hromada <hromada.dan@gmail.com>
2016-04-13 18:39:29 +0200

    Power socket wireframe

--- client/PowerSockets/index.js
+++ client/PowerSockets/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { Toggle } from 'material-ui';
 
+const sockets = [
+  { id: 1, label: 'Socket #1', value: true },
+  { id: 2, label: 'Socket #3', value: false },
+  { id: 4, label: 'Socket #4', value: false },
+  { id: 5, label: 'Socket #5', value: true },
+];
+
 class PowerSockets extends Component {
 
   render() {
     return (
-      <div className="grid-pad">
-        <Toggle label="Socket #1" labelPosition="right" />
-        <Toggle label="Socket #2" labelPosition="right" />
-        <Toggle label="Socket #3" labelPosition="right" />
-        <Toggle label="Socket #4" labelPosition="right" />
+      <div>
+        <h1>Power Sockets</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit, sed quia non numquam eius modi tempora
+          incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+        </p>
+        {sockets.map(socket => (
+          <Toggle
+            key={socket.id}
+            label={socket.label}
+            labelPosition="right"
+            style={{ marginTop: 20 }}
+            toggled={socket.value}
+          />
+        ))}
       </div>
     );
   }
