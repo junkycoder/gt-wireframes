7d000402c362a22fe8622ee309d806f3c4599d05
Dan Hromada <hromada.dan@gmail.com>
2016-04-17 17:31:18 +0200

    Init server

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 
-var env = process.env.NODE_ENV = (
-  process.argv[2] === 'production'
-) ?  'production' : 'development';
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development';
+}
+
+var env = process.env.NODE_ENV;
+var config = {
+
+  development: {
+    port: 8080
+  },
+
+  production: {
+    port: 80
+  }
+};
 
 if (env === 'development') {
   require('babel-register');
 }
 
+console.info("\n\n", 'INIT', env, config[env], 'Fok! xD', "\n\n");
+
 try {
-  require('./server');
+  require('./server').default(config[env]);
 }
 catch(e) {
   console.error('Server error:', e, "\n");
--- server/index.js
+++ server/index.js
@@ -4,15 +4,18 @@ import createHtml from './html';
 import createStatic from './static';
 import createDevel from './devel';
 
-const app = koa();
+function server ({ port }) {
+  const app = koa();
 
-if (app.env === 'development') {
-  app.use(createDevel());
+  if (app.env === 'development') {
+    app.use(createDevel());
+  }
+
+  app.use(mount('/static', createStatic()));
+
+  app.use(mount('/', createHtml()));
+
+  app.listen(port);
 }
 
-app.use(mount('/static', createStatic()));
-
-app.use(mount('/', createHtml()));
-
-
-app.listen(8080);
+export default server;
