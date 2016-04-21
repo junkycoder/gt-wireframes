6216229199250d95555c2906838acae2836d54e3
Dan Hromada <hromada.dan@gmail.com>
2016-04-21 10:58:24 +0200

    Bugfix 'devel middleware is broken #1'

--- server/html/index.js
+++ server/html/index.js
@@ -4,8 +4,12 @@ import render from './render';
 function main() {
   const app = koa();
 
-  app.use(function *() {
-    this.body = render();
+  app.use(function *(next) {
+    yield next;
+
+    if (this.status === 404) {
+      this.body = render();
+    }
   });
 
   return app;
